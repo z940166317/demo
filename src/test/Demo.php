7e6c804567479c2390d65d4450b18a54d82dcb63
zhi <940166317@qq.com>
2019-07-19 14:39:40 +0800

    init

---
+++ src/test/Demo.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Create by PhpStorm
+ * Author Aaronz
+ * Date: 2019-07-19
+ * Time: 13:59
+ */
+
+namespace xlauth\test;
+
+class Demo
+{
+
+    public function __construct()
+    {
+    }
+
+    public static function test(){
+        echo 'in test';
+    }
+
+}
